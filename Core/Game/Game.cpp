3192a35e8e626e59ca722930991757693ad01308
OA <OA@gl.com>
2025-03-19 05:49:54 +0200

    refactor Game class to singleton

--- Core/Game/Game.cpp
+++ Core/Game/Game.cpp
@@ -1,40 +1,53 @@
 #include "Game.hpp"
 #include <iostream>
 
-Game::Game() :
-    m_window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE)
-{
-    m_window.setFramerateLimit(120);
-    m_window.setVerticalSyncEnabled(false);
-   // m_window.setKeyRepeatEnabled(false);
+// Singleton instance accessor
+Game& Game::getInstance() {
+    static Game instance;
+    return instance;
 }
 
-void Game::update() {
-    // Update game state here
+Game::Game() {
+    this->initWindow();
 }
 
-void Game::handleEvents() {
-    while (m_window.pollEvent(m_event)) {
-        if (sf::Event::Closed == m_event.type)
-            m_window.close();
+Game::~Game() {
+}
+
+void Game::initWindow() {
+    //m_window = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
+    m_window = std::make_unique<sf::RenderWindow>(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
+    m_window->setFramerateLimit(120);
+    m_window->setVerticalSyncEnabled(false);
+    // m_window.setKeyRepeatEnabled(false);
+}
+
+void Game::handleEvents(void) {
+    while (m_window->pollEvent(m_event)) {
+        if (sf::Event::Closed == m_event.type) {
+            m_window->close();
+        }
     }
 }
 
+void Game::update(void) {
+    handleEvents();
+}
+
 void Game::run(void) {
-    while (m_window.isOpen()) {
-        m_dt = m_dtClock.restart();
-        handleEvents();
+    while (m_window->isOpen()) {
+        m_dt = m_dtClock.restart().asSeconds();
         update();
         render();
 #ifdef _WIN32
         system("cls");
 #endif // _WIN32
-        std::cout << "Delta time: " << m_dt.asSeconds() << "s\n";
+        std::cout << "Delta time: " << m_dt << "s\n";
     }
 }
 
 void Game::render(void) {
-    m_window.clear();
+    m_window->clear();
     // Draw game objects here
-    m_window.display();
+    m_window->display();
 }
--- Core/Game/Game.hpp
+++ Core/Game/Game.hpp
@@ -1,50 +1,58 @@
-#ifndef GAME_H
-#define GAME_H
+#ifndef GAME_HPP
+#define GAME_HPP
 
 #include <iostream>
 #include <string>
 
-#include <ctime>
 #include <cstdint>
 #include <cstdlib>
+#include <ctime>
 
 #include "SFML\Window.hpp"
 #include "SFML\Graphics.hpp"
 #include "SFML\Audio.hpp"
 #include "SFML\Network.hpp"
 
+#include "State.hpp"
 
 class Game {
 
 private:
-
-	// Methods
-	void handleEvents();
-	void update(void);
-	void render(void);
-
-	// Members
-	sf::RenderWindow m_window;
-	sf::Event m_event{};
-	sf::Clock m_dtClock;
-	sf::Time m_dt;
-
-	// Compile-time constants
-	static constexpr uint16_t WINDOW_WIDTH = 800;
-	static constexpr uint16_t WINDOW_HEIGHT = 600;
-	static constexpr const char* WINDOW_TITLE = "Game Title";
-
-public:
+	// Constructor
 	Game();
-	~Game() = default;
 
-	// Copy Prevention
+	// Delete copy constructor and assignment operator
 	Game(const Game&) = delete;
 	Game& operator=(const Game&) = delete;
 
 	// Methods
-	void run(void);
+	void initWindow();
 
+	void handleEvents(void);
+	void update(void);
+	void render(void);
+
+	// Members
+	std::unique_ptr<sf::RenderWindow> m_window;
+	sf::Event m_event{};
+	sf::Clock m_dtClock;
+
+	float m_dt;
+
+	// Compile-time constants
+	static constexpr uint32_t WINDOW_WIDTH = 800;
+	static constexpr uint32_t WINDOW_HEIGHT = 600;
+	static constexpr const char* WINDOW_TITLE = "Game Title";
+
+public:
+	// Destructor
+	virtual ~Game();
+
+	// // Singleton accessor
+	static Game& getInstance();
+
+	// Methods
+	void run(void);
 };
 
-#endif // GAME_H
+#endif // GAME_HPP
